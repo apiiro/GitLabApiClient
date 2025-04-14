020dcf00a1d01009c5b02cdafe88838d6df69e57
alexanderpalvanov <alex.p@apiiro.com>
2025-04-14 16:20:34 +0300

    wip

--- src/GitLabApiClient/Models/AccessLevel.cs
+++ src/GitLabApiClient/Models/AccessLevel.cs
@@ -1,8 +1,12 @@
 namespace GitLabApiClient.Models
 {
+    // per this documentation https://docs.gitlab.com/api/access_requests/#:~:text=The%20access%20levels%20are%20defined,Guest%20(%2010%20)
     public enum AccessLevel
     {
+        NoAccess = 0,
+        MinimalAccess = 5,
         Guest = 10,
+        Planner = 15,
         Reporter = 20,
         Developer = 30,
         Maintainer = 40,
